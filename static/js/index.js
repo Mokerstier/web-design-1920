94d05c4a7856992b04aef08b4de318f3def14438
Mokerstier <pannekoek-wouter@hotmail.com>
2020-04-22 15:08:08 +0200

    added loading state

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,25 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const results = document.querySelector(".results");
+const load = document.querySelector('img')
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const url = input.value;
-
+    load.classList.remove('display-none')
   fetch(`/${url}`)
-    .then((response) => {
+    .then((response) => { 
       return response.json();
     })
     .then((data) => {
       console.log(data);
       let message = ''
       results.innerHTML = ''
-
+        load.classList.add('display-none')
       const headerTitle = document.createElement('h2')
       const headerScore = document.createElement('h2')
       const tip = document.createElement('p')
-      const improv = document.createElement('h4')
+      const improv = document.createElement('h3')
 
       if(data.categories.accessibility.score >= 0.9){
         message = 'Dat is best een goede score!'
@@ -48,8 +49,9 @@ form.addEventListener("submit", (e) => {
               console.log(element[1].score)
             
             const container = document.createElement("div");
-            const audit = document.createElement("h3");
+            const audit = document.createElement("h4");
             const desc = document.createElement("p");
+            audit.classList.add('orange')
             audit.innerText = element[1].id;
             desc.innerHTML = element[1].description;
             
